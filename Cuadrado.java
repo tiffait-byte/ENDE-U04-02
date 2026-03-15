3a82bddebbc33a93b73e166bf2cdf171b08cdb80
Tiffany <tiffa@gmail.com>
2026-03-15 09:29:43 +0100

    Commit inicial: Clase Figuras sin modificar

---
+++ Cuadrado.java
@@ -0,0 +1,7 @@
+package figuras;
+import java.awt.Color;
+public class Cuadrado extends Rectángulo{
+public Cuadrado (double x, double y, Color color, double lado){
+super (x, y, color, lado, lado);
+}
+}
---
+++ Figura.java
@@ -0,0 +1,26 @@
+package figuras;
+import java.awt.Color;
+public abstract class Figura{
+private Punto centro;
+private Color color;
+public Figura(double x, double y, Color color){
+centro = new Punto (x, y);
+this.color = color;
+}
+public double getXCentro(){return centro.getX();}
+public double getYCentro(){return centro.getY();}
+public Color getColor(){return color;}
+public void setXCentro(double x){centro.setX (x);}
+public void setYCentro(double y){centro.setY (y);}
+public void setColor(Color color){this.color = color;}
+public abstract double perímetro();
+public abstract double área();
+public int esMayorQue (Figura otraFigura) {
+int codigo;
+if (this.área() > otraFigura.área()) codigo = 1;
+else
+if (this.área() < otraFigura.área()) codigo = -1;
+else codigo = 0;
+return codigo;
+}
+}
